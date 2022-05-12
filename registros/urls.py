e4f14d8fb94046a5c0dc88a0c4f3ad08d0fe666f
cristiantodi <toleko89@hotmail.com>
2022-05-12 16:12:53 -0500

    Casi terminado el login

--- registros/urls.py
+++ registros/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import VRegistro, CerrarSesion
+from .views import VRegistro, CerrarSesion, Logear
 
 urlpatterns = [
     path('', VRegistro.as_view(), name="Registros"),
     path('CerrarSesion',CerrarSesion, name="Cerrar_sesion"),
+    path('logear', Logear, name="logear"),
 
 ]
--- registros/views.py
+++ registros/views.py
@@ -2,18 +2,18 @@ from django.shortcuts import render, redirect
 
 from django.views.generic import View
 
-from django.contrib.auth.forms import UserCreationForm
-
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 
 from django.contrib import messages
+
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
 # Create your views here.
 
 class VRegistro(View):
     def get (self,request):
-        form=UserCreationForm()
+        form = UserCreationForm()
         return render(request,"registros/registros.html",{"form":form})
         pass
 
@@ -33,3 +33,22 @@ class VRegistro(View):
 def CerrarSesion(request):
     logout(request)
     return redirect('Home')
+
+def Logear(request):
+    if request.method=="POST":
+        form=AuthenticationForm(request,data=request.POST)
+        if form.is_valid():
+            nombreUsuario=form.cleaned_data.get("username")
+            contra=form.cleaned_data.get("password")
+            usuario=authenticate(username=nombreUsuario, password=contra)
+            if usuario is not None:
+                login(request, usuario)
+                return redirect('Home')
+            else:
+                messages.error(request, "Usuario no valido")
+        else:
+            messages.error(request, "Informacion incorrecta")
+    
+    form = AuthenticationForm()
+    return render(request, "login/login.html", {"form":form})
+
